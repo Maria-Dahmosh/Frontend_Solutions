461fa73fc6ff4b1674e59eb55a542455ef03e7ee
Maria-Dahmosh <mariadahmoush27@gmail.com>
2024-04-15 20:38:38 +0300

    js day2

--- week5-js/Day 1/11. Date & Times/index.js
+++ week5-js/Day 1/11. Date & Times/index.js
@@ -1,16 +1,46 @@
-//1
+//1  The output you're seeing is in ISO 8601 format
 const currentDate = new Date();
-const options = {
-  weekday: "short",
-  year: "numeric",
-  month: "short",
-  day: "numeric",
-  hour: "2-digit",
-  minute: "2-digit",
-  second: "2-digit",
-  hour12: false,
-};
+console.log(currentDate);
+
+//2
+const currentYear = currentDate.getFullYear();
+console.log(currentYear);
+
+//3 3 for April
+const currentMonth = currentDate.getMonth();
+console.log(currentMonth);
+
+//4
+const currentDay = currentDate.getDate();
+console.log(currentDay);
+
+//5  1 for Monday
+const currentWeekDay = currentDate.getDay();
+console.log(currentWeekDay);
+
+//6
+const currentHour = currentDate.getHours();
+const currentMinute = currentDate.getMinutes();
+const currentSecond = currentDate.getSeconds();
+console.log(currentSecond);
 console.log(
-  currentDate.toLocaleDateString(undefined, options) +
-    " (Eastern European Standard Time)"
+  `Hours:${currentHour}, Minutes:${currentMinute}, Seconds:${currentSecond}`
 );
+
+//7
+const numOfDays = new Date(currentYear, currentMonth + 1, 0).getDate();
+console.log(`${numOfDays}`);
+
+//8
+
+currentDate.setDate(currentDate.getDate() + 1);
+
+const tomorrowDate = currentDate;
+console.log(tomorrowDate);
+
+//9
+
+const totalMinutes = 90;
+const hours = Math.floor(totalMinutes / 60);
+const minutes = totalMinutes % 60;
+console.log(`90 minutes is equal to ${hours} hours and ${minutes} minutes`);
